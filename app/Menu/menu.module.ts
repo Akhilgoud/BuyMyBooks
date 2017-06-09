9ff36416a98e375b388bd167b0145a5cc3891930
Yeddula <yeddulav@ds.ad.adp.com>
2017-06-09 19:21:06 +0530

    c

--- app/Menu/menu.module.ts
+++ app/Menu/menu.module.ts
@@ -6,21 +6,11 @@ import { FormsModule } from '@angular/forms';
 import {CommonModule} from '@angular/common';
 import { ModalModule } from 'ngx-bootstrap/modal';
 @NgModule({
-<<<<<<< HEAD
-  imports: [MaterialModule,
-            AccordionModule,
-            MdIconModule,
-            PostModule,
-            displayBooksModule,
-            FormsModule,
-            RouterModule.forRoot([{path:'',component : displayBooksComponent },{path: 'post' , component : PostComponent }],{useHash : true})],
-=======
   imports: [CommonModule,
     FormsModule,
     RouterModule,
     ModalModule.forRoot()
   ],
->>>>>>> 76ba3fa8fdaa388e3d91877882de2064369f8804
   declarations: [
     MenuComponent
   ],
